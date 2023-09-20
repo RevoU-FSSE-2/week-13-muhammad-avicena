f6b4b8b6965186d1a5c374e8c74fa69362706e76
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-20 15:47:40 +0700

    feat: add addCategory function

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -2,16 +2,28 @@ import { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { useFetchList } from '../../hooks';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, MenuItem } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, MenuItem, Container, Paper, Typography } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 interface Category {
-    id: number;
+    id?: number;
     name: string;
     is_active: boolean;
 }
 
+interface AddCategory {
+    name: string;
+    is_active: boolean;
+}
+
+interface DataProfile {
+    name: string;
+    email: string;
+}
+
 const DashboardPage: React.FC = () => {
+    const [addModalOpen, setAddModalOpen] = useState(false);
+    const [addedRow, setAddedRow] = useState<AddCategory | null>({ name: '', is_active: false });
     const [editModalOpen, setEditModalOpen] = useState(false);
     const [editedRow, setEditedRow] = useState<Category | null>(null);
     const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
@@ -19,10 +31,33 @@ const DashboardPage: React.FC = () => {
 
     const validate = sessionStorage.getItem('userToken');
 
-    // Function to open the edit modal
+    const { data, loading, error } = useFetchList<Category[]>({
+        url: 'https://mock-api.arikmpt.com/api/category',
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${validate}`,
+        },
+    });
+
+    const { data: dataProfile } = useFetchList<DataProfile>({
+        url: 'https://mock-api.arikmpt.com/api/user/profile',
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${validate}`,
+        },
+    });
+
+    const handleAddModal = () => {
+        setAddModalOpen(true);
+    }
+
+    const handleCloseAddModal = () => {
+        setAddedRow(null);
+        setAddModalOpen(false);
+    }
+
     const handleEditData = (row: Category) => {
         setEditedRow(row);
-        console.log(row, "isi row edit");
         setEditModalOpen(true);
     };
 
@@ -41,15 +76,39 @@ const DashboardPage: React.FC = () => {
         setDeleteDialogOpen(false);
     };
 
-    const { data, loading, error } = useFetchList<Category[]>({
-        url: 'https://mock-api.arikmpt.com/api/category',
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${validate}`,
-        },
-    });
+    const handleAddCategory = () => {
+        console.log(addedRow, "isi added row")
+        axios.post('https://mock-api.arikmpt.com/api/category/create', {
+            name: addedRow?.name,
+            is_active: addedRow?.is_active,
+        }, { headers: { Authorization: `Bearer ${validate}` } })
+            .then((response) => {
+                handleCloseAddModal();
+                console.log('Add successful', response.data);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Add Successful',
+                    text: 'You have successfully added a new category.',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = '/dashboard';
+                    }
+                    setInterval(() => {
+                        window.location.href = '/dashboard';
+                    }, 3000);
+                });
+            }).catch((error) => {
+                console.log(error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Add Failed',
+                    text: 'An error occurred during add category. Please try again.',
+                })
+            })
+    }
 
     const handleDeleteCategory = () => {
+
         axios.delete(`https://mock-api.arikmpt.com/api/category/${rowToDelete?.id}`,
             { headers: { Authorization: `Bearer ${validate}` } })
             .then((response) => {
@@ -71,8 +130,6 @@ const DashboardPage: React.FC = () => {
     }
 
     const handleUpdateCategory = () => {
-
-        console.log(editedRow, "isi edited row")
 
         axios.put(`https://mock-api.arikmpt.com/api/category/update`, {
             id: editedRow?.id,
@@ -111,7 +168,7 @@ const DashboardPage: React.FC = () => {
             field: 'is_status',
             headerName: 'Status',
             width: 130,
-            renderCell: (params) => (<div>{params.row.is_active ? 'true' : 'false'}</div>)
+            renderCell: (params) => (<div>{params.row.is_active ? 'Active' : 'Deactive'}</div>)
         },
         {
             field: 'actions',
@@ -160,9 +217,61 @@ const DashboardPage: React.FC = () => {
         window.location.href = '/';
     }
 
+    // Handle logout
+    const handleLogout = () => {
+        sessionStorage.removeItem('userToken');
+        window.location.href = '/';
+    };
+
     return (
         <>
-            <Dialog open={editModalOpen} onClose={handleCloseEditModal}>
+            {/* Add Modal Dialog */}
+            <Dialog open={addModalOpen} onClose={handleCloseAddModal}>
+                <DialogTitle>Add Row</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="Name"
+                        style={{ margin: '10px 0' }}
+                        onChange={(e) => {
+                            setAddedRow((prevAddedRow) => ({
+                                name: e.target.value,
+                                is_active: prevAddedRow?.is_active || false,
+                            }));
+                        }}
+                        fullWidth
+                    />
+                    <TextField
+                        label="Status"
+                        select
+                        style={{ margin: '25px 0' }}
+                        onChange={(e) => {
+                            setAddedRow((prevAddedRow) => ({
+                                name: prevAddedRow?.name || '',
+                                is_active: e.target.value === 'active',
+                            }));
+                        }}
+                        fullWidth
+                    >
+                        <MenuItem value="active">Active</MenuItem>
+                        <MenuItem value="deactive">Deactive</MenuItem>
+                    </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseAddModal}>Cancel</Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            handleAddCategory();
+                        }}
+                    >
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog >;
+
+            {/* Edit Modal Dialog */}
+            < Dialog open={editModalOpen} onClose={handleCloseEditModal} >
                 <DialogTitle>Edit Row</DialogTitle>
                 <DialogContent>
                     <TextField
@@ -180,7 +289,7 @@ const DashboardPage: React.FC = () => {
                         label="Status"
                         select
                         style={{ margin: '25px 0' }}
-                        value={editedRow?.is_active ? 'active' : 'inactive'}
+                        value={editedRow?.is_active ? 'active' : 'deactive'}
                         onChange={(e) => {
                             if (editedRow) {
                                 setEditedRow({ ...editedRow, is_active: e.target.value === 'active' });
@@ -189,7 +298,7 @@ const DashboardPage: React.FC = () => {
                         fullWidth
                     >
                         <MenuItem value="active">Active</MenuItem>
-                        <MenuItem value="inactive">Inactive</MenuItem>
+                        <MenuItem value="deactive">Deactive</MenuItem>
                     </TextField>
                 </DialogContent>
                 <DialogActions>
@@ -204,11 +313,11 @@ const DashboardPage: React.FC = () => {
                         Save
                     </Button>
                 </DialogActions>
-            </Dialog>
+            </Dialog >
 
             {/* Delete Confirmation Dialog */}
-            <Dialog open={deleteDialogOpen} onClose={handleCloseDeleteDialog}>
-                <DialogTitle>Delete Row</DialogTitle>
+            < Dialog open={deleteDialogOpen} onClose={handleCloseDeleteDialog} >
+                <DialogTitle>Delete Category</DialogTitle>
                 <DialogContent>
                     Are you sure you want to delete this row?
                 </DialogContent>
@@ -224,7 +333,48 @@ const DashboardPage: React.FC = () => {
                         Delete
                     </Button>
                 </DialogActions>
-            </Dialog>
+            </Dialog >
+
+            <Container
+                sx={{
+                    width: "100%",
+                    height: 250,
+                }}
+            >
+                {/* Profile Data */}
+                <Paper elevation={3} style={{ padding: '20px' }}>
+                    <Typography variant="h6" gutterBottom>
+                        Profile Data
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                        Name: {dataProfile?.name}
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                        Email: {dataProfile?.email}
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={handleAddModal}
+                        style={{ margin: '10px 10px' }}
+                    >
+                        Add Category
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="error"
+                        onClick={handleLogout}
+                        style={{ margin: '20px 0' }}
+                    >
+                        Logout
+                    </Button>
+                </Paper>
+            </Container>
+
+            {/* Grid table data */}
+            <Typography variant="h5" gutterBottom>
+                List of Category
+            </Typography>
             <div style={{ height: 400, width: '100%' }}>
                 {data ? (
                     <DataGrid
