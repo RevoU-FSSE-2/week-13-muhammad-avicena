836828e2134359071b06c282db5381f140765110
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-21 10:46:01 +0700

    perf: optimize routing dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import { LoginPage, RegisterPage, DashboardPage } from './pages'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <LoginPage />
+    },
+    {
+      path: '/register',
+      element: <RegisterPage />
+    },
+    {
+      path: '/dashboard',
+      element: <DashboardPage />
+    }
+  ])
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </>
   )
 }
